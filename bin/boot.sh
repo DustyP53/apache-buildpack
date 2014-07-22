988090b93b1cf5d6cf9d0583724e5bff04c13492
Dusty Pressley <dusty.pressley@gmail.com>
2014-07-22 18:19:14 -0400

    Forgot to add the axm.conf file to $APACHE_ROOT/conf.d

--- bin/boot.sh
+++ bin/boot.sh
@@ -1,16 +1,16 @@
 export APACHE_ROOT=/home/vcap/app/httpd
 export INCLUDE_LINE="Include conf.d/*.conf"
+export AXM_LINE="Include \"/opt/rsa/axm/agent-50-apache/webservers/Apache_2.2.15/conf/ct-httpd.conf\""
 
 mv $APACHE_ROOT/conf/httpd.conf $APACHE_ROOT/conf/httpd.conf.org
 erb $APACHE_ROOT/conf/httpd.conf.org > $APACHE_ROOT/conf/httpd.conf
-
-cat $APACHE_ROOT/conf/httpd.conf.org > ./test.conf
 
 # 1. create conf.d directory
 # 2. add line to httpd.conf to include conf.d files
 # 3. add the modified axm.conf file.
 mkdir $APACHE_ROOT/conf.d
 echo $INCLUDE_LINE >> $APACHE_ROOT/conf.d/axm.conf
+echo $AXM_LINE >> $APACHE_ROOT/conf.d/axm.conf
 
 (tail $APACHE_ROOT/logs/*log &)
 exec $APACHE_ROOT/bin/httpd -DFOREGROUND
